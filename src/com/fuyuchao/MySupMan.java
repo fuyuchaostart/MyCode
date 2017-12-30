9e98e2b26dde1105cd3bba83489237312bb74cd2
fycServlet <2026893004@qq.com>
2017-12-30 09:24:03 +0800

    jdk动态代理

---
+++ src/com/fuyuchao/MySupMan.java
@@ -0,0 +1,10 @@
+package com.fuyuchao;
+
+/**
+ * @author fycstart 邮箱: fycstart@126.com
+ * @version 2017年12月30
+ *          天变不足畏，祖宗不足法，人言不足恤
+ */
+public interface MySupMan {
+    public int say(int x, int y);
+}
